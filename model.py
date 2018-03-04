ed8e24dd687df8b371cf41a7b7fe769409e2d304
Emin Mešić <>
2018-03-04 22:20:26 +0100

    Added new parameters to model and comments to some methods

--- model.py
+++ model.py
@@ -7,12 +7,21 @@ from mesa.datacollection import DataCollector
 from util import ArtisanType
 
 class ArtisanAgent(Agent):
-    def __init__(self, unique_id, model, knowledge):
+    def __init__(self, unique_id, model, lifetime, knowledge):
+        '''
+            Configure agent with input data
+        '''
         super().__init__(unique_id, model)
-        self.age = random.randrange(15, self.model.average_lifetime)
+        self.lifetime = lifetime
+        self.age = random.randrange(15, self.lifetime)
         self.knowledge = knowledge
+        self.learn_ability = random.choice([True, False])
+        self.teach_ability = random.choice([True, False])
 
     def get_title(self):
+        '''
+            Method helps us to check the person in the execution of other methods
+        '''
         if self.knowledge < 0.4:
             return ArtisanType.APPRENTICE
         elif self.knowledge >= 0.4 and self.knowledge < 0.6:
@@ -21,6 +30,9 @@ class ArtisanAgent(Agent):
             return ArtisanType.MENTOR
 
     def step(self):
+        '''
+            Method called for every step of our agent
+        '''
         self.move()
         self.knowledge_transfer()
         self.age += 1
@@ -30,6 +42,9 @@ class ArtisanAgent(Agent):
             self.model.schedule.remove(self)
 
     def move(self):
+        '''
+            Method set the configuration of moving our agent over the grid
+        '''
         if self.get_title() == ArtisanType.MENTOR:
             return
 
@@ -48,36 +63,61 @@ class ArtisanAgent(Agent):
 class ArtisanModel(Model):
     description = 'A model for simulating Artisan and Learner relation.'
 
-    def __init__(self, width, height, initial_artisan_mentor, initial_artisan_student, average_lifetime):
+    def __init__(self, width, height, initial_artisan_mentor, initial_artisan_student, step_time, average_lifetime):
+        '''
+            Constructor method of our model
+            Create model and configure with model parameters
+        '''
         self.height = height
         self.width = width
         self.initial_artisan_mentor = initial_artisan_mentor
         self.initial_artisan_student = initial_artisan_student
+        self.step_time = step_time
         self.average_lifetime = average_lifetime
 
         self.schedule = RandomActivation(self)
         self.grid = MultiGrid(self.height, self.width, torus=True)
 
+        # agent must have unique id
         unique_id = 0
-        # Set up mentors
+
+        '''
+            Set up mentors with specific configuration
+            Configure unique id
+            Generate random lifetime
+            Create agent
+            Set newly created agent to the random position on grid and allow schedule
+        '''
         for i in range(self.initial_artisan_mentor):
             unique_id += 1
-            artisan = ArtisanAgent(unique_id, self, 0.6)
+            lifetime = random.randrange(self.average_lifetime - 10, self.average_lifetime + 10)
+            artisan = ArtisanAgent(unique_id, self, lifetime, 0.6)
             self.grid.place_agent(artisan, (random.randrange(self.width), random.randrange(self.height)))
             self.schedule.add(artisan)
 
-        # Set up other agents
+        '''
+            Set up other agents
+            Configure unique id
+            Generate random lifetime
+            Create agent
+            Set newly created agent to the random position on grid and allow schedule
+        '''
         for i in range(initial_artisan_student):
             unique_id += 1
-            artisan = ArtisanAgent(unique_id, self, random.uniform(0, 0.4))
+            lifetime = random.randrange(self.average_lifetime - 10, self.average_lifetime + 10)
+            artisan = ArtisanAgent(unique_id, self, lifetime, random.uniform(0, 0.4))
             self.grid.place_agent(artisan, (random.randrange(self.width), random.randrange(self.height)))
             self.schedule.add(artisan)
         
         self.running = True
 
     def step(self):
+        '''
+            method executed for every step triggered automatically or manually
+        '''
         self.running = True
-        self.schedule.step()
+        self.schedule.step() #this method called same method of agent
 
+        # some specific logic on model level
         if self.schedule.get_agent_count() == 0:
             self.running = False
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from model import ArtisanAgent, ArtisanModel
 from util import ArtisanType
 
 def artisan_portrayal(agent):
+    # grid people configuration
     portrayal = {"scale": 0.9, "Layer": 1}
 
     if agent.get_title() == ArtisanType.APPRENTICE:
@@ -17,15 +18,17 @@ def artisan_portrayal(agent):
 
     return portrayal
 
+# grid configuration
 grid_width = 10
 grid_height = 10
-
 grid = CanvasGrid(artisan_portrayal, grid_width, grid_height, 600, 600)
 
+# configure model params and form input values
 model_params = {"width": grid_width,
                 "height": grid_height,
                 "initial_artisan_mentor": UserSettableParameter('slider', 'Intial artisan mentors', 2, 1, 15),
                 "initial_artisan_student": UserSettableParameter('slider', 'Intial artisan student', 10, 1, 50),
+                "step_time": UserSettableParameter('slider', 'Step time (month)', 6, 3, 12, 3),
                 "average_lifetime": UserSettableParameter('slider', 'Average lifetime', 65, 50, 100)}
 
 server = ModularServer(ArtisanModel, [grid], "Artisan Learner Relation", model_params)
