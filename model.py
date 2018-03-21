a1b7c619c51642db9643c033ee7936766c8b6e9a
Emin Mešić <>
2018-03-22 00:24:39 +0100

    Changed logic of apprentice and education

--- model.py
+++ model.py
@@ -7,45 +7,36 @@ from mesa.datacollection import DataCollector
 from util import ArtisanType
 
 class ArtisanAgent(Agent):
-    def __init__(self, unique_id, model, lifetime, knowledge):
+    def __init__(self, unique_id, model, type, lifetime, age, knowledge):
         '''
             Configure agent with input data
         '''
         super().__init__(unique_id, model)
+        self.type = type
         self.lifetime = lifetime
-        self.age = random.randrange(15, self.lifetime) * 12 # month value
+        self.age = age
         self.knowledge = knowledge
         self.learn_ability = random.uniform(0, 1)
         self.teach_ability = random.uniform(0, 1)
-
-    def get_title(self):
-        '''
-            Method helps us to check the person in the execution of other methods
-        '''
-        if self.knowledge < 0.4:
-            return ArtisanType.APPRENTICE
-        elif self.knowledge >= 0.4 and self.knowledge < 0.6:
-            return ArtisanType.MASTER
-        elif self.knowledge >= 0.6:
-            return ArtisanType.MENTOR
+        self.affinity = random.uniform(0, 1)
+        self.education = 0 # max education if 4 years (in months)
 
     def step(self):
         '''
             Method called for every step of our agent
         '''
+        self.age += self.model.step_time / 12.0
+        self.education += self.model.step_time / 12.0
+
         self.move()
         self.knowledge_transfer()
-        self.age += self.model.step_time
-    
-        if self.age > (self.model.average_lifetime * 12):
-            self.model.grid._remove_agent(self.pos, self)
-            self.model.schedule.remove(self)
+        self.check_lifetime()
 
     def move(self):
         '''
             Method set the configuration of moving our agent over the grid
         '''
-        if self.get_title() == ArtisanType.MENTOR:
+        if self.type == ArtisanType.MENTOR or self.type == ArtisanType.APPRENTICE:
             return
 
         possible_steps = self.model.grid.get_neighborhood(
@@ -56,6 +47,10 @@ class ArtisanAgent(Agent):
         self.model.grid.move_agent(self, new_position)
 
     def knowledge_transfer(self):
+        if self.education >= 4 and self.knowledge < 0.2:
+            self.remove_agent()
+            return
+
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) >= 2:
             self.knowledge += 0.05
@@ -65,10 +60,21 @@ class ArtisanAgent(Agent):
             self.knowledge += 0.001
         pass
 
+    def check_lifetime(self):
+        '''
+            Method check lifetime of agent and removes from model
+        '''
+        if self.age >= self.lifetime:
+            self.remove_agent()
+
+    def remove_agent(self):
+        self.model.grid._remove_agent(self.pos, self)
+        self.model.schedule.remove(self)
+
 class ArtisanModel(Model):
     description = 'A model for simulating Artisan and Learner relation.'
 
-    def __init__(self, width, height, initial_artisan_mentor, initial_artisan_student, step_time, average_lifetime):
+    def __init__(self, width, height, initial_artisan_mentor, initial_artisan_apprentice, min_affinity, step_time, average_lifetime):
         '''
             Constructor method of our model
             Create model and configure with model parameters
@@ -76,7 +82,8 @@ class ArtisanModel(Model):
         self.height = height
         self.width = width
         self.initial_artisan_mentor = initial_artisan_mentor
-        self.initial_artisan_student = initial_artisan_student
+        self.initial_artisan_apprentice = initial_artisan_apprentice
+        self.min_affinity = min_affinity
         self.step_time = step_time
         self.average_lifetime = average_lifetime
 
@@ -96,7 +103,8 @@ class ArtisanModel(Model):
         for i in range(self.initial_artisan_mentor):
             unique_id += 1
             lifetime = random.randrange(self.average_lifetime - 10, self.average_lifetime + 10)
-            artisan = ArtisanAgent(unique_id, self, lifetime, 0.6)
+            age = float(random.randrange(15, lifetime))
+            artisan = ArtisanAgent(unique_id, self, ArtisanType.MENTOR, lifetime, age, 0.6)
             self.grid.place_agent(artisan, self.grid.find_empty())
             self.schedule.add(artisan)
 
@@ -107,22 +115,54 @@ class ArtisanModel(Model):
             Create agent
             Set newly created agent to the random position on grid and allow schedule
         '''
-        for i in range(initial_artisan_student):
+        for i in range(initial_artisan_apprentice):
             unique_id += 1
             lifetime = random.randrange(self.average_lifetime - 10, self.average_lifetime + 10)
-            artisan = ArtisanAgent(unique_id, self, lifetime, random.uniform(0, 0.4))
+            artisan = ArtisanAgent(unique_id, self, ArtisanType.APPRENTICE, lifetime, 15.0, 0)
             self.grid.place_agent(artisan, (random.randrange(self.width), random.randrange(self.height)))
             self.schedule.add(artisan)
         
         self.running = True
+        self.is_started = False
 
     def step(self):
         '''
             method executed for every step triggered automatically or manually
         '''
         self.running = True
+        if not self.is_started:
+            self.sort_apprentice()
+            self.is_started = True
+
         self.schedule.step() #this method called same method of agent
 
         # some specific logic on model level
         if self.schedule.get_agent_count() == 0:
             self.running = False
+    
+    def sort_apprentice(self):
+        mentor_agents = [x for x in self.schedule.agents if x.type == ArtisanType.MENTOR]
+        apprentice_agents = [x for x in self.schedule.agents if x.type == ArtisanType.APPRENTICE]
+
+        mentor_agents = sorted([x for x in self.schedule.agents if x.type == ArtisanType.MENTOR], key=lambda m: m.affinity, reverse=True)
+        apprentice_agents = sorted([x for x in self.schedule.agents if x.type == ArtisanType.APPRENTICE], key=lambda m: m.affinity, reverse=False)
+
+        for mentor in mentor_agents:
+            mentor_position = mentor.pos
+
+            for i in range(5):
+                if not apprentice_agents:
+                    break
+
+                apprentice = apprentice_agents.pop()
+                if apprentice.affinity >= self.min_affinity:
+                    possible_positions = self.grid.get_neighborhood(
+                        mentor_position,
+                        moore=True,
+                        include_center=False)
+                    new_position = random.choice(possible_positions)
+                    self.grid.move_agent(apprentice, new_position)
+
+        if apprentice_agents:
+           for apprentice in apprentice_agents:
+                apprentice.remove_agent()
--- server.py
+++ server.py
@@ -9,25 +9,26 @@ def artisan_portrayal(agent):
     # grid people configuration
     portrayal = {"scale": 0.9, "Layer": 1}
 
-    if agent.get_title() == ArtisanType.APPRENTICE:
+    if agent.type == ArtisanType.APPRENTICE:
         portrayal["Shape"] = "images/apprentice.png"
-    elif agent.get_title() == ArtisanType.MASTER:
+    elif agent.type == ArtisanType.MASTER:
         portrayal["Shape"] = "images/master.png"
-    elif agent.get_title() == ArtisanType.MENTOR:
+    elif agent.type == ArtisanType.MENTOR:
         portrayal["Shape"] = "images/mentor.png"
 
     return portrayal
 
 # grid configuration
-grid_width = 10
-grid_height = 10
-grid = CanvasGrid(artisan_portrayal, grid_width, grid_height, 600, 600)
+grid_width = 5
+grid_height = 5
+grid = CanvasGrid(artisan_portrayal, grid_width, grid_height, 400, 400)
 
 # configure model params and form input values
 model_params = {"width": grid_width,
                 "height": grid_height,
                 "initial_artisan_mentor": UserSettableParameter('slider', 'Intial artisan mentors', 2, 1, 15),
-                "initial_artisan_student": UserSettableParameter('slider', 'Intial artisan student', 10, 1, 50),
+                "initial_artisan_apprentice": UserSettableParameter('slider', 'Intial artisan apprentice', 10, 1, 50),
+                "min_affinity": UserSettableParameter('slider', 'Minimum affinity', 0.2, 0.0, 1, 0.1),
                 "step_time": UserSettableParameter('slider', 'Step time (month)', 6, 3, 12, 3),
                 "average_lifetime": UserSettableParameter('slider', 'Average lifetime', 65, 50, 100)}
 
